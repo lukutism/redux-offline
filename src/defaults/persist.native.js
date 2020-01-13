292879ff1a109d90798d01e11d6051bfff47638a
Lukas <lukutim@gmail.com>
2020-01-13 11:38:35 +0200

    native async

--- src/defaults/persist.native.js
+++ src/defaults/persist.native.js
@@ -2,11 +2,7 @@
 
 // $FlowIgnore
 import { persistStore } from 'redux-persist';
-<<<<<<< HEAD
-import AsyncStorage from '@react-native-community/async-storage'; // eslint-disable-line
-=======
 import AsyncStorage from '@react-native-community/async-storage';
->>>>>>> 7adef48... replace RN AsyncStorage with community package
 
 export default (store: any, options: {}, callback: any) =>
   persistStore(store, { storage: AsyncStorage, ...options }, callback);
